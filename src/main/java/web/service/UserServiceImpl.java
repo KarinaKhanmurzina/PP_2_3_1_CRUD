94c6d6c6ef7dcbf5716dc47bcf04b9188bf432b9
Karina <khanmurzinakarina17@gmail.com>
2022-03-28 18:47:38 +0300

    1st solution

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional
+    @Override
+    public List<User> getAllUser() {
+        return userDao.getAllUser();
+    }
+
+    @Transactional
+    @Override
+    public void deleteUserById(int id) {
+        userDao.deleteUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public User updateUser(User user) {
+        return userDao.updateUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Transactional
+    @Override
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+}
